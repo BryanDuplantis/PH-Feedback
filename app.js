1eb3c74d2767c5eca1811424e842e108651e4758
Bryan Duplantis <duplantis@gmail.com>
2015-10-10 22:13:00 -0500

    created new db, added entries

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+//require PostgreSQL
 var pg = require("pg");
-var conString = "pg://admin:guest@localhost:5432/Customers";
+
+//connect to 'customers' database on PostgreSQL
+var conString = "postgres://localhost:5432/customers";
 
 var client = new pg.Client(conString);
 client.connect();
 
-client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
-client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Bryan', 'Duplantis']);
-client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Ed', 'Bush']);
-client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Zoe', 'Ames']);
-client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Bob', 'Paterno']);
+//creates new table
+client.query("CREATE TABLE IF NOT EXISTS cstmrs(firstname varchar(64), lastname varchar(64))");
+client.query("INSERT INTO cstmrs(firstname, lastname) values($1, $2)", ['Bryan', 'Duplantis']);
+client.query("INSERT INTO cstmrs(firstname, lastname) values($1, $2)", ['Ed', 'Bush']);
+client.query("INSERT INTO cstmrs(firstname, lastname) values($1, $2)", ['Zoe', 'Ames']);
+client.query("INSERT INTO cstmrs(firstname, lastname) values($1, $2)", ['Bob', 'Paterno']);
 
-var query = client.query("SELECT firstname, lastname FROM emps ORDER BY lastname, firstname");
+var query = client.query("SELECT firstname, lastname FROM cstmrs ORDER BY lastname, firstname");
 query.on("row", function (row, result) {
     result.addRow(row);
 });
