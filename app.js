7e27fa8d17f852e952231cd50d1d5d810e0b8728
Bryan Duplantis <duplantis@gmail.com>
2015-10-12 15:18:51 -0500

    added customers route, renders client data from Postgres as JSON

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var customer = require('./controllers/clients');
 
 app.set('view engine', 'ejs');
 
-//body-parser returns middleware that only parses JSON
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use('/', hello)
-app.use('/customers', customers)
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({'extended':'false'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 //routes
+app.get('/', hello)
 app.get('/customers', customer.getCustomers);
 
 //initializing a port
--- controllers/clients.js
+++ controllers/clients.js
@@ -1,9 +1,7 @@
 var Customer = require('../models/customer');
 
 exports.getCustomers = function(req, res) {
-  Customer.find(function (err, users) {
-    if (err) return console.error(err);
-    console.log(users);
-    res.send(users);
+  Customer.find(function (clients) {
+    res.send(clients);
   });
 };
